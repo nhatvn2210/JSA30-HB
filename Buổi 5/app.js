2980e16fd454c3f40ad5e4cd58ad0318e05514ac
nhatvn2210 <coiminhnhat2210@gmail.com>
2023-11-12 11:03:38 +0700

    Lesson 5

--- Buổi 5/app.js
+++ Buổi 5/app.js
@@ -1,5 +1,21 @@
-// Variable
-// Array
-// Object
 // Function
+
+function getID(x) {
+    return document.getElementById(x);
+}
+
+// Variable
+
+const mainTitle = getID('main-title');
+
+// Array
+
+// Object
+
+
 // Main script
+console.log(mainTitle);
+
+mainTitle.style.color = "red";
+
+mainTitle.innerText = "JSA - 30: TODO List";
