4c7e8cb515f251377436e8bf1704737ea5539470
nhatvn2210 <coiminhnhat2210@gmail.com>
2023-11-12 12:08:37 +0700

    Lesson 5

--- Buổi 5/app.js
+++ Buổi 5/app.js
@@ -1,21 +1,46 @@
-// Function
+// Shortcut function
 
-function getID(x) {
+function getByID(x){
     return document.getElementById(x);
 }
+// Function
 
+function addTodo(selectedInput, event) {
+    event.preventDefault();
+    const todoText = selectedInput.value;
+    return todoText;
+}
+
+function renderTodoList(selectedList) {
+    let htmls = "" ;
+
+    for (let i in selectedList) {
+        htmls += `<li> ${selectedList[i]} </li>`;
+    }
+
+    return htmls;
+}
 // Variable
 
-const mainTitle = getID('main-title');
+const todoInput = getByID('todo-input');
+const addButton = getByID('add-button');
+const renderList = getByID('result-list');
 
 // Array
 
+let todoList = JSON.parse(localStorage.getItem('todo-list')) || [];
+
 // Object
 
-
 // Main script
-console.log(mainTitle);
 
-mainTitle.style.color = "red";
+renderList.innerHTML = renderTodoList(todoList);
 
-mainTitle.innerText = "JSA - 30: TODO List";
+addButton.addEventListener('click', function(event){
+    todoList.push(addTodo(todoInput , event));
+    localStorage.setItem('todo-list' , JSON.stringify(todoList));
+    renderList.innerHTML = renderTodoList(todoList);
+})
+
+
+
